e8062621e3cb600c75f48d52ae3e50345aab367c
Yegor Kozlovskiy <basterfly@gmail.com>
2020-01-30 15:37:03 +0200

    added SafeArea, Row, Container with images

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,9 +10,58 @@ void main() {
           backgroundColor: Colors.orange,
         ),
         backgroundColor: Colors.blueGrey,
-        body: Center(
-          child: Image(
-            image: AssetImage('images/GDFlutterTitle.png'),
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              Container(
+                child: Image(
+                  image: AssetImage('images/GDFlutterTitle.png'),
+                ),
+              ),
+              Container(
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      child: Image(
+                        image: AssetImage('images/heart.png'),
+                      ),
+                    ),
+                    Container(
+                      child: Image(
+                        image: AssetImage('images/cloud.png'),
+                      ),
+                    ),
+                    Container(
+                      child: Image(
+                        image: AssetImage('images/DirrectButton.png'),
+                      ),
+                    ),
+                    Container(
+                      height: 44,
+                      width: 130,
+                      color: Colors.white,
+                    ),
+                    Container(
+                      child: Image(
+                        image: AssetImage('images/flag.png'),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    Container(
+                      height: 50,
+                      width: 150,
+                      color: Colors.green,
+                    ),
+                  ],
+                ),
+              )
+            ],
           ),
         ),
       ),
