081747918e3aaa1fc3df29d693217758f312ce1f
Yegor Kozlovskiy <basterfly@gmail.com>
2020-04-16 12:35:23 +0300

    First App. Learned widgets:  Scaffold, Center, Text, Center, Image, NetworkImage and some its properties.

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,16 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Center(
-      child: Text("Hello!"),
-    ),
-  ));
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("App Bar Widjet"),
+            backgroundColor: Colors.blueGrey,
+          ),
+          backgroundColor: Colors.blueGrey[900],
+          body: Center(
+            child: Image(
+              image: NetworkImage(
+                  'https://www.premierguitar.com/ext/resources/images/content/2015_09/Rig-Rundowns/Social-Distortion/Ness-Guitar1-WEB.jpg?t=1441809233&width=900'),
+            ),
+          ))));
 }
