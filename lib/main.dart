3dbd9bfb72feaf95f949c7000d35c66c59e2a51a
Yegor Kozlovskiy <basterfly@gmail.com>
2020-04-16 23:52:26 +0300

    First App. pubspec.yaml - assets: images in 2 ways. Adding of Image was changed from NetworkImage to AssetImage. Added application icons.

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
+  runApp(
+    MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: Text("App Bar Widjet"),
-            backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text("App Bar Widget"),
+          backgroundColor: Colors.blueGrey,
+        ),
+        backgroundColor: Colors.blueGrey[900],
+        body: Center(
+          child: Image(
+            image: AssetImage('images/guitar.png'),
           ),
-          backgroundColor: Colors.blueGrey[900],
-          body: Center(
-            child: Image(
-              image: NetworkImage(
-                  'https://www.premierguitar.com/ext/resources/images/content/2015_09/Rig-Rundowns/Social-Distortion/Ness-Guitar1-WEB.jpg?t=1441809233&width=900'),
-            ),
-          ))));
+        ),
+      ),
+    ),
+  );
 }
